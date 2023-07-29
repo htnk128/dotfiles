da84ad383a577b9835df70d9345a2cacead767af
Hiroaki Tanaka <hiroaki.tanaka128@gmail.com>
2023-07-29 12:37:34 +0900

    Update init.sh

--- vscode/init.sh
+++ vscode/init.sh
@@ -3,11 +3,9 @@
 set -eu
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-VSCODE_SET_DIR="${HOME}/Library/Application Support/Code/User"
 
-# Link settings.json
-if [ -L "${VSCODE_SET_DIR}/settings.json" ]; then
-  ln -fsvn "${SCRIPT_DIR}/settings.json" "${VSCODE_SET_DIR}/settings.json"
+if [ -e ~/Library/Application\ Support/Code/User ]; then
+  ln -fsvn "${SCRIPT_DIR}/settings.json" ~/Library/Application\ Support/Code/User
 fi
 
 # Install extensions using the code command
